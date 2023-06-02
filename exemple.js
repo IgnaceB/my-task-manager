c9845a691a75758f22411fdc5c8a3f8b1e1dc1f7
IgnaceB <ignacebernard@hotmail.com>
2023-06-02 16:34:53 +0200

    finalv3000

---
+++ exemple.js
@@ -0,0 +1,4 @@
+let tasks = ["task1", "task2", "task3"]
+let check = ["not yet", "not yet", "not yet"]
+console.log(tasks)
+console.log(check)
--- manager.js
+++ manager.js
@@ -5,91 +5,98 @@ const fs = require('fs');
 const readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-let tasks = ["task1", "task2", "task3"]
-let check=["not yet","not yet","not yet"]
-
-localStorage.setItem('myStorage', JSON.stringify(obj));
+let rawdata1 = fs.readFileSync('data1.json')
+let rawdata2 = fs.readFileSync('data2.json')
+let tasks = JSON.parse(rawdata1)
+let check = JSON.parse(rawdata2)
 
 const showtasks = () => {
-    for (let i=0;i<tasks.length;i++){
-        console.log("Nbr"+i+" "+tasks[i]+" status : "+check[i])
+    for (let i = 0; i < tasks.length; i++) {
+        console.log("Nbr" + i + " " + tasks[i] + " status : " + check[i])
     }
 }
 const addtasks = () => {
     rl.question("What's the name of the new task ?", (name) => {
         tasks.push(name)
         check.push("not yet")
-        console.log("task :"+name+" created") 
+        console.log("task :" + name + " created")
         menu()
     }
     )
 }
-const deletetasks=() =>{
-    for (let i=0;i<tasks.length;i++){
-        console.log("Nbr"+i+" "+tasks[i]+" status : "+check[i])
+const deletetasks = () => {
+    for (let i = 0; i < tasks.length; i++) {
+        console.log("Nbr" + i + " " + tasks[i] + " status : " + check[i])
     }
     rl.question("what is the Nbr of the task do you want to delete ?", (nb) => {
         delete tasks[nb]
         delete check[nb]
-        console.log("task :"+nb+" deleted") 
+        console.log("task :" + nb + " deleted")
         menu()
     }
     )
 }
-const checktasks=() =>{ 
-    for (let i=0;i<tasks.length;i++){ if (check[i]=="not yet") {
-
-        console.log("Nbr"+i+" "+tasks[i])
-    }
+const checktasks = () => {
+    for (let i = 0; i < tasks.length; i++) {
+        if (check[i] == "not yet") {
+            console.log("Nbr" + i + " " + tasks[i])
+        }
         continue
     }
     rl.question("what is the Nbr of the task you finished ?", (nb) => {
-        check[nb]="done"
-        console.log("task :"+nb+" finished") 
+        check[nb] = "done"
+        console.log("task :" + nb + " finished")
         menu()
     }
     )
-
 
 }
 
 const menu = () => {
-    // fs.readFile('data.json','utf-8', (err,data)=> {
+ 
+    tasks = tasks.filter(String)
+    check = check.filter(String)
+    fs.readFile('welcome.txt', 'utf-8', (err, data) => {
+        if (err) throw err;
+        console.log(data.toString());
+    })
 
-    // }
-    // )
-    tasks=tasks.filter(String)
-    check=check.filter(String)
-fs.readFile('welcome.txt', 'utf-8', (err, data) => {
-    if (err) throw err;
+    rl.question('', (option) => {
+        switch (option) {
+            case "1":
+                showtasks()
+                menu()
+                break
 
-    console.log(data.toString());
-})
+            case "2":
+                addtasks()
+                break
 
-rl.question('', (option) => {
-    switch (option) {
-        case "1":
-            showtasks()
-            menu()
-            break
+            case "3":
+                deletetasks()
+                break
 
-        case "2":
-            addtasks()
-            break
+            case "4":
+                checktasks()
+                break
 
-        case "3":
-            deletetasks()
-            break
+            case "5":
+                let data1=JSON.stringify(tasks)
+                let data2=JSON.stringify(check)
+                fs.writeFile('data1.json', data1, err => {
+                    if (err) {
+                      console.error(err);
+                    }
+                })
+                fs.writeFile('data2.json', data2, err => {
+                    if (err) {
+                      console.error(err);
+                    }
+                })
+                rl.close()
+                break
+        }
 
-        case "4":
-            checktasks()
-            break
-
-        case "5":
-            rl.close()
-            break
-    }
-
-})
+    })
 }
 menu()
